40dc2f5cdebbbe2926d85f968f8e550c725c8d77
Evgeniy <skaterzub@yandex.ru>
2026-01-27 20:53:12 +0300

    new practice files

---
+++ Lesson_3/practice.py
@@ -0,0 +1,11 @@
+""" name = 'Alice, petr, 35'
+print('p' in name)
+print('P' in name)
+print('35' in name) """
+
+
+number = 10
+number1 = 11
+print(number / 2)
+print(number1 // 2)
+print(number1 % 2)
---
+++ Lesson_4/practice.py
@@ -0,0 +1,17 @@
+""" my_list = []
+my_list.append(1)
+my_list.append('text')
+my_list.append(1.567)
+print(my_list)
+popo = my_list.pop(-1)
+print(popo)
+
+ """
+
+""" number = 100
+number = number +20 
+number +=15
+print(number) """
+
+text = input('who are you today? ')
+print(text, 'hello')
